c11a39cbe83936a54543f41e829be5ae54ffa62e
go2collage <91822422+go2collage@users.noreply.github.com>
2023-03-11 22:38:44 +0530

    Add files via upload

---
+++ BP_P11.py
@@ -0,0 +1,12 @@
+# Basic Python Program
+
+# Find Leap Year 
+
+year = int(input("Enter any Year: "))
+
+if year % 4 == 0:       # by using year % 4, check Leap Year
+    print(year," is Leap Year.")
+
+else:                   # Otherwise not Leap Year
+    print(year," is not Leap Year.")
+
